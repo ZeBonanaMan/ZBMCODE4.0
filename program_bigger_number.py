4e13a3216ff621f26a679717a58842637486f7da
ZeBonanaMan <mcoygaldiano@gmail.com>
2024-01-06 16:22:43 +0800

    window

--- program_bigger_number.py
+++ program_bigger_number.py
@@ -1,6 +1,12 @@
 import tkinter
 from tkinter import Tk
 
+def biggest_num():
+    num1 = input()
+    num2 = input()
+    num3 = input()
+    
+
 win=Tk()
 win.title("if-else statement")
 win.geometry("200x200")
