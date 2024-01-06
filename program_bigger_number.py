815ca0ff14479122aa99f7602b34d5429cc0925d
ZeBonanaMan <mcoygaldiano@gmail.com>
2024-01-06 17:09:14 +0800

    adding color and display

--- program_bigger_number.py
+++ program_bigger_number.py
@@ -1,14 +1,32 @@
-import tkinter
-from tkinter import Tk
+import tkinter as tk
+from tkinter import font
+
 
 def biggest_num():
-    num1 = input()
-    num2 = input()
-    num3 = input()
-    
+    try:
+        num1 = input()
+        num2 = input()
+        num3 = input()
 
-win=Tk()
+    except ValueError:
+        display_label.config(text="Please enter a valid number.")
+
+win=tk.Tk()
 win.title("if-else statement")
-win.geometry("200x200")
-win.mainloop()
+win.geometry("1000x1000")
 
+#darkmode parameters
+bg_color = "#2a2a2a"
+fg_color = "#FFFFFF"
+label_color = "#2d9dbd"
+input_bg_color = "#545454"
+button_bg_color = "#2a2a2a"
+apple_color = "#ed2939"
+money_color  ="#3cb043"
+
+win.config(bg=bg_color)
+
+display_label = tk.Label(bg=bg_color)
+display_label.pack()
+
+win.mainloop()
