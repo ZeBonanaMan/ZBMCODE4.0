f1a5feb0a58164d9ef29ab41715782adc040df6f
ZeBonanaMan <mcoygaldiano@gmail.com>
2024-01-06 21:19:17 +0800

    Added the equation and revised the inputs for tkinter

--- program_bigger_number.py
+++ program_bigger_number.py
@@ -2,12 +2,29 @@ import tkinter as tk
 from tkinter import font
 
 
-def biggest_num():
+def calculate():
     try:
-        num1 = input()
-        num2 = input()
-        num3 = input()
-
+        num_1 = number1_entry.get()
+        num_2 = number2_entry.get()
+        num_3 = number3_entry.get()
+    
+    #phase1
+        if num_1 <= num_2:
+            temp_biggest = num_2
+            if temp_biggest <= num_3:
+                    temp_biggest = num_3
+                    display_label.config(text=f"ang pinakamataas ay: {temp_biggest}")
+            else:
+                display_label.config(text=f"ang pinakamataas ay: {temp_biggest}")
+        else: 
+            temp_biggest = num_1
+            if temp_biggest <= num_3:
+                    temp_biggest = num_3
+                    display_label.config(text=f"ang pinakamataas ay: {temp_biggest}")
+            else:
+                display_label.config(text=f"ang pinakamataas ay: {temp_biggest}")
+            
+        
     except ValueError:
         display_label.config(text="Please enter a valid number.")
 
@@ -22,6 +39,24 @@ label_color = "#2d9dbd"
 input_bg_color = "#545454"
 
 win.config(bg=bg_color)
+
+number1_label = tk.Label()
+number1_label.pack
+number1_entry = tk.Entry()
+number1_entry.pack()
+
+number2_label = tk.Label()
+number2_label.pack
+number2_entry = tk.Entry()
+number2_entry.pack()
+
+number3_label = tk.Label()
+number3_label.pack
+number3_entry = tk.Entry()
+number3_entry.pack()
+
+calculate_button = tk.Button(text="Calculate", command=calculate,)
+calculate_button.pack()
 
 display_label = tk.Label(bg=bg_color)
 display_label.pack()
